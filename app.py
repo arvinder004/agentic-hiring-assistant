4a314452cb22fda22211b4f1df4b5fa660d08826
arvinder004 <asdhoul004@gmail.com>
2026-01-07 00:42:43 +0530

    added basic chat ui

---
+++ app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import google.generativeai as genai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+
+
+model = genai.GenerativeModel('gemini-2.5-flash-lite')
+
+st.title("TalentScout Hiring Assistant")
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("Type your message..."):
+    st.chat_message("user").markdown(prompt)
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    response = model.generate_content(prompt)
+    
+    with st.chat_message("assistant"):
+        st.markdown(response.text)
+    st.session_state.messages.append({"role": "assistant", "content": response.text})
